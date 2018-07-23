9395d8384cf94958340723cb40591d4df5c6b25d
Alex Souza <alexsouza8045@gmail.com>
2018-07-23 14:40:55 -0300

    1 commit

---
+++ src/java/criaPDF/PDF.java
@@ -0,0 +1,53 @@
+package criaPDF;
+// GUANABARA É O REI 
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Rectangle;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.FileOutputStream;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import org.primefaces.PrimeFaces;
+
+@ManagedBean
+public class PDF {
+
+    private String nome;
+    private String conteudo;
+
+    public void convertePdf() {
+
+        Document document = new Document();
+
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream("C:\\Projetos\\NOVO\\criaPDF\\PDF\\" + nome + ".pdf"));
+            document.open();
+            Paragraph texto = new Paragraph(conteudo);
+            document.add(texto);
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Arquivo " + getNome(), "Convertido com Sucesso.");
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+        } catch (Exception e) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Algo deu errado, tente novamente. ", "" + e);
+            PrimeFaces.current().dialog().showMessageDynamic(message);
+            System.out.println(e);
+        }
+        document.close();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+}
